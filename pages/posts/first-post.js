cf834b8604e53518422ce37722116ce380c213c4
Remi <remi.petit93370@gmail.com>
2022-09-04 14:24:34 +0200

    components

---
+++ pages/posts/first-post.js
@@ -0,0 +1,30 @@
+import Head from 'next/head'
+import Link from 'next/link';
+import Script from 'next/script';
+
+import Layout from '../../components/layout';
+
+import styles from '../../styles/FirstPost.module.scss'
+
+export default function FirstPost() {
+  return (
+    <Layout>
+      <Head>
+        <title>First Post</title>
+      </Head>
+      <Script
+        src="https://connect.facebook.net/en_US/sdk.js"
+        strategy="lazyOnload"
+        onLoad={() =>
+          console.log(`script loaded correctly, window.FB has been populated`)
+        }
+      />
+      <h1>First Post</h1>
+      <h2>
+        <Link href="/">Back to home</Link>
+      </h2>
+      <br/>
+      <img src="/images/profile.jpg" alt="image" />
+    </Layout>
+  );
+}
